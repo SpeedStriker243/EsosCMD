70e583f522e52c83be78e7c50bcd54715a294602
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-03-02 15:13:35 +0000

    Update kernel.py

--- kernel.py
+++ kernel.py
@@ -1,9 +1,4 @@
 import time, random, getpass, datetime, webbrowser, os, platform
+import EsosInbuilt
 
-osname = "EsosCMD"
-print(f"Now loading {osname}…")
-time.sleep(5)
-if platform.system == "Darwin":
-    print(f"Running {osname} on macOS with Darwin kernel version {str(platform.release())} - ", str(time.strftime("%a %d %b %Y")))
-else:
-    print(f"Running {osname} on {str(platform.system())} {str(platform.release())} - ", str(time.strftime("%a %d %b %Y")))
+EsosInbuilt.startup()
