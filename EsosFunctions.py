6a5a6be7781e99a92f763c0a6b972b9a25780451
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-03-08 12:54:29 +0000

    Add EsosScript functionality

--- EsosFunctions.py
+++ EsosFunctions.py
@@ -25,3 +25,35 @@ def StartupMessage():
         print(f"Running EsosCMD on {str(platform.system())} {str(platform.release())} - " + str(time.strftime("%a %d %b %Y")))
         if getpass.getuser() == "root":
             print("You're logged in as root, so please be careful.")
+			
+def EsosScript(filename):
+	try:
+		ScriptFile = open(filename + ".ess")
+		ScriptLine = ""
+		LineCount = 0
+			
+		print(f"Reading {ScriptFile.name}...")
+		while ScriptLine != "end":
+			LineCount += 1
+			ScriptLine = ScriptFile.readline()
+			
+			if LineCount == 1 and ScriptLine[:7] != "escript":
+				print(f"Syntax error. An EsosScript file must start with version definition.")
+				break
+			if ScriptLine[:3] == "///":
+				pass
+			elif ScriptLine[:3] == "log":
+				print(ScriptLine[4:])
+			elif ScriptLine[:7] == "escript":
+				print(f"Using EsosScript version {ScriptLine[8:]}")
+			elif ScriptLine == "":
+				pass
+			elif ScriptLine[:3] == "end":
+				break
+			else:
+				print(f"Syntax error. The line '{ScriptLine}' was not understood.")
+				break
+				
+		ScriptFile.close	
+	except FileNotFoundError:
+		print(f"The script file {Arg1}.ess was not found.")
